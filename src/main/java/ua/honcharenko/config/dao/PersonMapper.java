e9962fcf9f373ebc18554db1b0d95fa832682eed
honcharenko1489 <honcharenko1489@gmail.com>
2021-10-02 00:16:33 +0300

    first  crud project

---
+++ src/main/java/ua/honcharenko/config/dao/PersonMapper.java
@@ -0,0 +1,20 @@
+package ua.honcharenko.config.dao;
+
+import org.springframework.jdbc.core.RowMapper;
+import ua.honcharenko.config.model.Person;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class PersonMapper implements RowMapper<Person> {
+
+    @Override
+    public Person mapRow(ResultSet resultSet, int i) throws SQLException {
+        Person person = new Person();
+        person.setId(resultSet.getInt("id"));
+        person.setName(resultSet.getString("name"));
+        person.setAge(resultSet.getInt("age"));
+        person.setEmail(resultSet.getString("email"));
+        return person;
+    }
+}
